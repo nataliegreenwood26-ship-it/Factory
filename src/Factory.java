ef23cb7195a2e6ea89cb54bc596ac839ab35b376
nataliegreenwood <natalie_greenwood26@milton.edu>
2025-11-13 10:57:57 -0500

    Classwork

--- src/Factory.java
+++ src/Factory.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.impl.orbutil.HexOutputStream;
+
 public class Factory {
 
     //instance variables
@@ -16,15 +18,23 @@ public class Factory {
     //constructor
     public Factory(){
         System.out.println("We make houses");
-        yearFounded = 2025;
+        yearFounded = 2012;
         isOpen = false;
         earnings = 1.1;
         products = "houses";
         factoryInfo();
+        factoryInfo();
+
+        House h1= new House();
+        h1.owner= "Mr.Kim";
+        System.out.println("the owner is" + h1.owner);
+        h1.size=200;
+        h1.walls=10;
+        h1.address= "126 manet road";
+
     }
 
     public void factoryInfo() {
-
 
 
     }
---
+++ src/House.java
@@ -0,0 +1,16 @@
+public class House {
+
+
+    //instance variables
+    //1. at the top of the class
+    //2. they can be used anywhere
+    //3. they have the word public before
+
+    public int walls;
+    public double size;
+    public String owner;
+    public String address;
+    public boolean isOccupied;
+
+
+}
